07a0376ba755c616dfd9efbaa824001542125828
KITt0S <rostys.gb@gmail.com>
2026-05-09 12:43:24 +0300

    feat: added Time class

---
+++ src/main/java/com/funnubunny/app/core/Input.java
@@ -0,0 +1,26 @@
+package com.funnubunny.app.core;
+
+import com.jogamp.newt.event.KeyEvent;
+import com.jogamp.newt.event.KeyListener;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Input implements KeyListener {
+
+    private static final Set<Short> pressedKeys = new HashSet<>();
+
+    @Override
+    public void keyPressed(KeyEvent event) {
+        pressedKeys.add(event.getKeyCode());
+    }
+
+    @Override
+    public void keyReleased(KeyEvent event) {
+        pressedKeys.remove(event.getKeyCode());
+    }
+
+    public static boolean isKeyPressed(short keyCode) {
+        return pressedKeys.contains(keyCode);
+    }
+}
