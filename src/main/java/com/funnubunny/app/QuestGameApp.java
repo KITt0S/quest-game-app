6799a2047c637546a0c50e3095df903ae1067157
KITt0S <rostys.gb@gmail.com>
2026-05-09 12:19:05 +0300

    feat: mvn setup

---
+++ src/main/java/com/funnubunny/app/QuestGameApp.java
@@ -0,0 +1,9 @@
+package com.funnubunny.app;
+
+public class QuestGameApp {
+
+    public static void main(String[] args) {
+        GameEngine engine = new GameEngine();
+        engine.start();
+    }
+}
