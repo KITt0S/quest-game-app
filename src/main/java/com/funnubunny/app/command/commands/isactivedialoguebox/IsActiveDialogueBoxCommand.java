630cc697ea890cfc99bd3633080e3c29cfc29b78
KITt0S <rostys.gb@gmail.com>
2026-05-30 18:22:23 +0300

    fix: removed redundant commands

--- src/main/java/com/funnubunny/app/command/commands/isactivedialoguebox/IsActiveDialogueBoxCommand.java
+++
@@ -1,6 +0,0 @@
-package com.funnubunny.app.command.commands.isactivedialoguebox;
-
-import com.funnubunny.app.command.commands.GameCommand;
-
-public class IsActiveDialogueBoxCommand implements GameCommand {
-}
--- src/main/java/com/funnubunny/app/dialoguebox/DialogueBoxSystem.java
+++ src/main/java/com/funnubunny/app/dialoguebox/DialogueBoxSystem.java
@@ -2,10 +2,7 @@ package com.funnubunny.app.dialoguebox;
 
 import com.funnubunny.app.command.*;
 import com.funnubunny.app.command.commands.*;
-import com.funnubunny.app.command.commands.isactivedialoguebox.IsActiveDialogueBoxAnswer;
-import com.funnubunny.app.command.commands.isactivedialoguebox.IsActiveDialogueBoxCommand;
 import com.funnubunny.app.command.commands.nextdialogue.NextDialogueCommand;
-import com.funnubunny.app.command.commands.showdialogue.ShowDialogueCommand;
 import com.funnubunny.app.state.WorldStateService;
 
 public class DialogueBoxSystem {
@@ -15,9 +12,7 @@ public class DialogueBoxSystem {
     public DialogueBoxSystem(DialogueBox dialogueBox, WorldStateService worldStateService, CommandBus commandBus) {
         this.dialogueBox = dialogueBox;
         this.worldStateService = worldStateService;
-        commandBus.register(IsActiveDialogueBoxCommand.class, this::isActiveDialogueBox);
         commandBus.register(InteractWithNpcCommand.class, this::interactWithNpc);
-        commandBus.register(ShowDialogueCommand.class, this::showDialogue);
         commandBus.register(NextDialogueCommand.class, this::next);
     }
 
@@ -26,15 +21,6 @@ public class DialogueBoxSystem {
             dialogueBox.show(worldStateService.getNpc().getDialogue());
         }
 
-        return new VoidAnswer();
-    }
-
-    private IsActiveDialogueBoxAnswer isActiveDialogueBox(IsActiveDialogueBoxCommand command) {
-        return new IsActiveDialogueBoxAnswer(dialogueBox.isActive());
-    }
-
-    private VoidAnswer showDialogue(ShowDialogueCommand gameCommand) {
-        dialogueBox.show(gameCommand.getDialogue());
         return new VoidAnswer();
     }
 
